ee995e9a14f61d005e8de71f39460b43ba93c141
Mehmet <kutlugmk@vcu.edu>
2022-05-04 08:38:47 -0400

    Added raspicam library

---
+++ libraries/raspicam-master/build/utils/CMakeFiles/raspicam_still_test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/network_drive/final/EGRE347_Final_Project/libraries/raspicam-master/utils/raspicam_still_test.cpp" "/home/pi/network_drive/final/EGRE347_Final_Project/libraries/raspicam-master/build/utils/CMakeFiles/raspicam_still_test.dir/raspicam_still_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../dependencies"
+  "../dependencies/mmal"
+  "../dependencies/vcos"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/network_drive/final/EGRE347_Final_Project/libraries/raspicam-master/build/src/CMakeFiles/raspicam.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
