5915c0dd8c7bdeea3cab272daba38aa02a23c0fc
Dan-yelp <dan.j.youngk@gmail.com>
2022-05-04 09:48:33 -0400

    Cleaning

---
+++ .history/Tracking_Module_py/Tracking_Module_20220503160436.py
@@ -0,0 +1,3 @@
+import sys
+
+sys.path.append('\Final_Project\EGRE347_Final_Project\EGRE347_Final_Project\Sensor_py\Sensor_class.py')
---
+++ .history/Tracking_Module_py/Tracking_Module_20220503160636.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.path.append('\Final_Project\EGRE347_Final_Project\EGRE347_Final_Project\Sensor_py\Sensor_class.py')
+
+import module
+# import RPi.GPIO as GPIO
+# from time import sleep
+import sys
+from signal import signal, SIGINT
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('CTRL-C detected. Exiting from Sensor_test.py')
+    exit(0)
+
+signal(SIGINT, handler)
+
+motion_sensor = Sensor_class.Sensor()
+
+motion_sensor.sensor_init()
+
+while(1):
+    motion = motion_sensor.sense_motion()
+    if(motion):
+        print('Motion\n')
+    else:
+        print('No motion\n')
+
+motion_sensor.sensor_shutdown()
